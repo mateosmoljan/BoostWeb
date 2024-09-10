8dc5badff24326b77b9ae770c688bf9dc566d2c5
SudoDosu <93739034+SudodoSu@users.noreply.github.com>
2024-09-10 18:47:01 +0200

    add

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,43 +1,42 @@
-// import { client } from "@/sanity/lib/client";
-// import { MetadataRoute } from "next";
-// import { Post } from "./utils/interface";
+import { client } from "@/sanity/lib/client";
+import { MetadataRoute } from "next";
+import { Post } from "./utils/interface";
 
-// export default async function sitemap():Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  async function getPosts() {
+    const query = `
+    *[_type == "post"] {
+      title,
+      slug,
+      publishedAt,
+      excerpt,
+      tags[]-> {
+        _id,
+        slug,
+        name
+      }
+    }
+    `;
+    const data = await client.fetch(query);
+    return data;
+  }
 
-//   async function getPosts() {
-//     const query = `
-//     *[_type == "post"] {
-//       title,
-//       slug,
-//       publishedAt,
-//       excerpt,
-//       tags[]-> {
-//         _id,
-//         slug,
-//         name
-//       }
-//     }
-//     `;
-//     const data = await client.fetch(query);
-//     return data;
-//   }
+  const posts: Post[] = await getPosts();
 
-//   const posts: Post[] = await getPosts();
+  const postUrls = posts.map((post) => ({
+    url: `https://next-cms-blog-ce.vercel.app/${post.slug.current}`,
+    lastModified: new Date(post.publishedAt),
+  }));
 
-//   const postUrls = posts.map((post) => ({
-//     url: `https://next-cms-blog-ce.vercel.app/${post.slug.current}`,
-//     lastModified: new Date(post.publishedAt),
-//   }))
-
-//   return [
-//     {
-//       url: `https://next-cms-blog-ce.vercel.app/`,
-//       lastModified: new Date(),
-//     },
-//     {
-//       url: "https://next-cms-blog-ce.vercel.app/tag",
-//       lastModified: new Date(),
-//     },
-//     ...postUrls,
-//   ]
-// }
+  return [
+    {
+      url: `https://next-cms-blog-ce.vercel.app/`,
+      lastModified: new Date(),
+    },
+    {
+      url: "https://next-cms-blog-ce.vercel.app/tag",
+      lastModified: new Date(),
+    },
+    ...postUrls,
+  ];
+}
